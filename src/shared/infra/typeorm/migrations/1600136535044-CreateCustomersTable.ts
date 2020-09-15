2a52ed5daaec7532b8262b36d744488a58cf655b
Thiago Bignotto <bignotto@gmail.com>
2020-09-15 00:07:00 -0300

    create new customer done

--- src/shared/infra/typeorm/migrations/1600136535044-CreateCustomersTable.ts
+++ src/shared/infra/typeorm/migrations/1600136535044-CreateCustomersTable.ts
@@ -18,6 +18,10 @@ export class CreateCustomersTable1600136535044 implements MigrationInterface {
             type: 'varchar',
           },
           {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
             name: 'created_at',
             type: 'timestamp with time zone',
             default: 'now()',
