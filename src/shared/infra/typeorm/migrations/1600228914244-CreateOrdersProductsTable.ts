0b5f3b99f6c7a039a68317b8df2f58e38306bcd4
Thiago Bignotto <bignotto@gmail.com>
2020-09-16 01:02:39 -0300

    wip: creating ordersproducts migration

---
+++ src/shared/infra/typeorm/migrations/1600228914244-CreateOrdersProductsTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateOrdersProductsTable1600228914244 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
